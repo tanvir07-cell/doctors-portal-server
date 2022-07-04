832bd074b0a68917bbd5dc4d6aadbdffba8954fb
unknown <tr9836859@gmail.com>
2022-07-04 14:39:17 +0600

    for the available services

--- index.js
+++ index.js
@@ -36,6 +36,46 @@ async function run() {
       res.send(services);
     });
 
+    // Warning: This is not the proper way to query multiple collection.
+    // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+    app.get("/available", async (req, res) => {
+      // step 1:  get all services
+      const services = await serviceCollection.find().toArray();
+
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+      const date = req.query.date;
+
+      const query = { date: date };
+      // eikhane suppose 31may teh kon kon service gula nitesi sob gula ekti array er moddeh thakbe:
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3: for each service(eikhane ami kon  service ti niechilam  31may teh seigula alada alada kora)
+      services.forEach((service) => {
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        // step 5: select slots for the service Bookings: ['', '', '', '']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        // step 6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        //step 7: set available to slots to make it easier
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
+    // for booking get api:
+    app.get("/booking", async (req, res) => {
+      const patientEmail = req.query.patientEmail;
+      const query = { patientEmail: patientEmail };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     // for booking post:
     app.post("/booking", async (req, res) => {
       const booking = req.body;
